0646c6dc640c1f3cee6992c81e14bb0f11f4b7ce
gaojunhao <1976223524@qq.com>
2021-01-25 23:47:02 +0800

    frontend: add column above home page
    add column above home page
    
    Signed-off-by: gaojunhao <1976223524@qq.com>

--- wechat-houses-rent-master/demo/pages/home/home.js
+++ wechat-houses-rent-master/demo/pages/home/home.js
@@ -6,6 +6,73 @@ Page({
    * 页面的初始数据
    */
   data: {
+            // 获取设备高度
+            appHeight: '',
+
+            // 筛选导航栏数据
+            msgList: [
+                { key: 1, name: '位置'},
+                { key: 2, name: '价格'},
+                { key: 3, name: '房型'},
+                { key: 4, name: '租赁方式'}
+            ],
+            // 判断导航栏列表是否显示
+            meunShow: [
+                { isShows: true },
+                { isShows: true },
+                { isShows: true },
+                { isShows: true }
+            ],
+    
+            // 区域数据
+            // 西安市区
+            areaLise: [
+                { id: 0, name: '不限' },
+                { id: 0, name: '地铁' },
+                { id: 1, name: '区域' }
+            ],
+            // 市区街道
+            rowLise: {
+                row_ditie: { id: 0, name: ['1号线', '2号线', '3号线', '4号线', '5号线', '6号线', '7号线', '8号线', '9号线', '10号线', '11号线', '12号线', '13号线', '14号线', '15号线', '16号线', '17号线', '浦江线'] },
+                row_quyu: { id: 1, name: ['黄浦', '静安', '长宁', '普陀', '徐汇', '浦东', '杨浦', '虹口', '宝山', '闵行', '松江', '嘉定', '青浦', '奉贤', '崇明', '金山'] }
+            },
+    
+            // 区域模块市区街道隐藏/显示
+            rowShow: [
+                { isShows: true },
+                { isShows: true },
+                { isShows: true },
+            ],
+    
+            // 区域右侧是否显示
+            rigShow: true,
+    
+            // 售价
+            price: [
+                { id: 0, name: '不限' },
+                { id: 1, name: '1千以下' },
+                { id: 2, name: '1 - 2千' },
+                { id: 3, name: '2 - 3千' },
+                { id: 4, name: '3 - 4千' },
+                { id: 5, name: '4 - 5千' },
+                { id: 6, name: '5 - 6千' },
+                { id: 7, name: '6 - 7千' },
+                { id: 8, name: '7千以上' },
+            ],
+    
+            // 房间型号
+            roomModel: [
+                { id: 0, name: '不限' },
+                { id: 1, name: '一室' },
+                { id: 2, name: '二室' },
+                { id: 3, name: '三室' },
+                { id: 4, name: '四室' },
+                { id: 5, name: '五室以上' }
+            ],
+            rentType: [
+              { id: 0, name: '整租' },
+              { id: 1, name: '合租' }
+          ],
     houses: [],
     all_houses: [],
     down: 0,
@@ -251,6 +318,100 @@ Page({
     wx.navigateTo({
       url: '../post_house/post_house',
     })
-  }
+  },
+    // 筛选导航栏事件
+    menuClick: function(e){
+      // 获取通过wxml  data-hi="{{ idx }}" 传过来的索引
+      var menuNum = e.currentTarget.dataset.hi;
 
+      // 拼接 ，使我们可以获取到menuShow里面每一个isSHows
+      var menuSrc = "meunShow[" + menuNum + "].isShows";
+
+      // 循环data中设置的meunShow
+      for (var n = 0; n < this.data.meunShow.length; n++){
+          // 拼接 ，使我们可以获取到menuShow里面每一个isSHows
+          var menuSrcs = "meunShow[" + n + "].isShows";
+          // 解决重复点击不能隐藏的问题
+          if (n != menuNum){
+              // 初始化，每次点击时先全部隐藏，但是重复点击不会隐藏
+              this.setData({
+                  [menuSrcs]: true
+              });
+          };
+      };
+
+      // 给当前点击的去反data中设置的meunShow，使之显示， 只写此处只会显示不能隐藏
+      this.setData({
+          [menuSrc]: !this.data.meunShow[e.currentTarget.dataset.hi].isShows
+      });
+  },
+
+  // 区域列表事件
+  rowClick: function(e){
+      // 限制第一个 '不限' 不能点击
+      if (e.currentTarget.dataset.hi == 0){
+      var menuSrc = "meunShow[0].isShows";
+        this.setData({
+          [menuSrc]: true
+      });
+    };
+      if (e.currentTarget.dataset.hi != 0){
+          // 获取wxml  data-hi="{{ index }}" 传过来的索引
+          var rowNum = e.currentTarget.dataset.hi;
+          // 同筛选导航栏事件，因第一个为不限不可点击， 所以减一
+          var rowSrc = "rowShow[" + ( rowNum - 1 ) + "].isShows";
+          // 隐藏所有的三级菜单
+          for (var m = 0; m < this.data.rowShow.length; m++){
+              var rowSrcs = "rowShow[" + m + "].isShows";
+              this.setData({
+                  [rowSrcs]: true
+              });
+          };
+          // 同上
+          this.setData({
+              rigShow: false,
+              [rowSrc]: !this.data.rowShow[e.currentTarget.dataset.hi].isShows
+          });
+      };
+  },
+ditieClick: function(e){
+        var rowNum = e.currentTarget.dataset.hi;
+        console.log(rowNum);
+        var menuSrc = "meunShow[0].isShows";
+        this.setData({
+          [menuSrc]: true
+      });
+},
+quyuClick: function(e){
+        var rowNum = e.currentTarget.dataset.hi;
+        console.log(rowNum);
+        var menuSrc = "meunShow[0].isShows";
+        this.setData({
+          [menuSrc]: true
+      });
+},
+priceClick: function(e){
+  var rowNum = e.currentTarget.dataset.hi;
+  console.log(rowNum);
+  var menuSrc = "meunShow[1].isShows";
+  this.setData({
+    [menuSrc]: true
+});
+},
+roomModelClick: function(e){
+  var rowNum = e.currentTarget.dataset.hi;
+  console.log(rowNum);
+  var menuSrc = "meunShow[2].isShows";
+  this.setData({
+    [menuSrc]: true
+});
+},
+rentTypeClick: function(e){
+  var rowNum = e.currentTarget.dataset.hi;
+  console.log(rowNum);
+  var menuSrc = "meunShow[3].isShows";
+  this.setData({
+    [menuSrc]: true
+});
+},
 })
